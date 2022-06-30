719a03e1ccbf4ac93fd4f728dfa34008de3cb56d
kotoriefreet <1063377232@qq.com>
2022-06-30 19:41:11 +0800

    动态绑定style

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,25 @@
 <template>
     <div>
-        <ul>
-            <li :class="{'active': index === currentIndex}" v-for="(item,index) in navs" :key="index" 												@click="changeFn(index)">大学起点</li>
-        </ul>
+        <!-- 动态绑定style 语法 v-bind:style="对象" 
+  			v-bind:style = '{css属性名: 值}'-->
+       <p v-bind:style="{color: 'red', fontSize: '20px'}">vue 动态绑定style11</p>
+       <p v-bind:style="{color: colorValue, fontSize: `${fontSize}px`}">vue 动态绑定style22</p>
     </div>
-</template> 
-
+</template>
 <script>
 export default {
 
-  data() {
-        return {
-            navs: ['大学起点', '高中起点', '初中起点', '小学起点'],
-            currentIndex: 0
-        };
-    },
-    methods: {
-        changeFn (index) {
-            this.currentIndex = index;
-        }
+   data() {
+       return {
+           colorValue: 'green',
+           fontSize: 22,
+       }
     }
+
 }
 </script>
 
 
 <style>
-  ul {
-    list-style: none;
-    border-radius: 10px;
-    width: 400px;
-    overflow: hidden;
-    padding: 0;
-  }
-  ul li {
-    float: left;
-    width: 100px;
-    height: 40px;
-    background-color: #ccc;
-    color: #fff;
-    text-align: center;
-    line-height: 40px;
-    cursor: pointer;
-  }
-  li.active {
-    background-color: blue;
-  }
+
 </style>
